948188aae7c5a10cb29509ee166662e11a5fc62b
Douglas Maalongaye <douglastawile@gmail.com>
2024-05-16 13:06:50 +0000

    Some files updated

--- src/users/ProfileTabs.tsx
+++ src/users/ProfileTabs.tsx
@@ -4,16 +4,20 @@ export function ProfileTabs() {
   return (
     <Tabs aria-label="Pills" style="pills" className="lg:flex justify-between">
       <Tabs.Item active title="Posts" className="lg:w-1/4">
-        <p className="text-sm text-gray-500 dark:text-gray-400">Content 1</p>
+        <p className="text-sm text-gray-500 dark:text-gray-400">Posts</p>
       </Tabs.Item>
       <Tabs.Item title="About" className="lg:w-1/4">
-        <p className="text-sm text-gray-500 dark:text-gray-400">Content 2</p>
+        <p className="text-sm text-gray-500 dark:text-gray-400">About</p>
       </Tabs.Item>
       <Tabs.Item title="Followings" className="lg:w-1/4">
-        <p className="text-sm text-gray-500 dark:text-gray-400">Content 3</p>
+        <p className="text-sm text-gray-500 dark:text-gray-400">
+          Content for Followings
+        </p>
       </Tabs.Item>
       <Tabs.Item title="Followers" className="lg:w-1/4">
-        <p className="text-sm text-gray-500 dark:text-gray-400">Content 4</p>
+        <p className="text-sm text-gray-500 dark:text-gray-400">
+          Content for Followers
+        </p>
       </Tabs.Item>
     </Tabs>
   );
