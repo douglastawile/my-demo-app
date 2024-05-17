16dcfee04c0ac0f190a254354b6681496c724a2d
Douglas Maalongaye <douglastawile@gmail.com>
2024-05-17 12:42:59 +0000

    Updating Profile Tabs

--- src/users/ProfileTabs.tsx
+++ src/users/ProfileTabs.tsx
@@ -1,10 +1,12 @@
 import { Tabs } from "flowbite-react";
+import PostsPage from "../posts/PostsPage";
 
 export function ProfileTabs() {
   return (
     <Tabs aria-label="Pills" style="pills" className="lg:flex justify-between">
       <Tabs.Item active title="Posts" className="lg:w-1/4">
         <p className="text-sm text-gray-500 dark:text-gray-400">Content 1</p>
+        <PostsPage />
       </Tabs.Item>
       <Tabs.Item title="About" className="lg:w-1/4">
         <p className="text-sm text-gray-500 dark:text-gray-400">Content 2</p>
